65097bb5f41685b18251824e506ed962582ec98a
ntzolov <ntzolov@gmail.com>
2023-01-16 18:23:48 +0200

    Update 07.....spiralMatrix.js

--- SoftUni JavaScript fundamentals/03.Arrays/03.Arrays exercises more/07.....spiralMatrix.js
+++ SoftUni JavaScript fundamentals/03.Arrays/03.Arrays exercises more/07.....spiralMatrix.js
@@ -1,21 +1,44 @@
 // This seems to be very hard. Waiting for more lectures...
 
-function spiralMatrix(num1, num2) {
-  let row = num1;
-  let col = num2;
-  transpose();
-  function transpose(matrix) {
-    const grid = [];
-    for (let j = 0; j < col; j++) {
-      grid[j] = Array(row);
+function spiralMatrix(row, col) {
+  let n = row * col;
+  let arr = Array.from({ length: n }, () => []);
+  let roww = 0;
+  let coll = 0;
+  let rowEnd = n - 1;
+  let colEnd = n - 1;
+  let counter = 1;
+
+  while (coll <= colEnd && roww <= rowEnd) {
+    // Top row & middle value (Where col === colEnd && row === rowEnd)
+    for (let i = coll; i <= colEnd; i++) {
+      arr[roww][i] = counter;
+      counter++;
     }
-    for (let i = 0; i < row; i++) {
-      for (let j = 0; j < col; j++) {
-        grid[j][i] = matrix[i][j];
-      }
+    roww++;
+
+    // end column
+    for (let i = roww; i <= rowEnd; i++) {
+      arr[i][colEnd] = counter;
+      counter++;
     }
-    console.log(grid);
+    colEnd--;
+
+    // end row
+    for (let i = colEnd; i >= coll; i--) {
+      arr[rowEnd][i] = counter;
+      counter++;
+    }
+    rowEnd--;
+
+    // First col from end
+    for (let i = rowEnd; i >= roww; i--) {
+      arr[i][coll] = counter;
+      counter++;
+    }
+    coll++;
   }
+  console.log(arr);
 }
 
 spiralMatrix(5, 5);
