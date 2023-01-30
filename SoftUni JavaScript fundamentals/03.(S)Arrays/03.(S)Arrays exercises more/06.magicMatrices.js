97aedcd0e033b69b48de35add3f7f546b537cdde
ntzolov <ntzolov@gmail.com>
2023-01-30 08:44:30 +0200

    Update

--- SoftUni JavaScript fundamentals/03.(S)Arrays/03.(S)Arrays exercises more/06.magicMatrices.js
+++ SoftUni JavaScript fundamentals/03.(S)Arrays/03.(S)Arrays exercises more/06.magicMatrices.js
@@ -2,12 +2,10 @@ function magicMatrices(arr) {
   let isEqual = true;
   let tempNumOne = 0;
   let tempNumTwo = 0;
-  let rowsSum = 0;
-  let columnsSum = 0;
 
   for (let i = 0; i < arr.length; i++) {
     tempNumOne = 0;
-    for (let j = 0; j < arr[i].length; j++) {
+    for (let j = 0; j < arr.length; j++) {
       tempNumOne += arr[i][j];
       if (i === 0) {
         tempNumTwo += arr[i][j];
@@ -18,7 +16,6 @@ function magicMatrices(arr) {
       break;
     }
   }
-  tempNumTwo = rowsSum
   tempNumOne = 0;
   tempNumTwo = 0;
 
@@ -35,9 +32,8 @@ function magicMatrices(arr) {
       break;
     }
   }
-  tempNumTwo = columnsSum
 
-  if (!isEqual || rowsSum !== columnsSum) {
+  if (!isEqual) {
     console.log('false');
   } else {
     console.log('true');
