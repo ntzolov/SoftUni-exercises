0f6f64b9c5cc720e42ee039e315a7ce57588b3a3
ntzolov <ntzolov@gmail.com>
2023-03-21 23:44:22 +0200

    Update

---
+++ SoftUni JavaScript fundamentals/Previuos exams exercises/Final exam/theImitationGame.js
@@ -0,0 +1,45 @@
+function theImitationGame(input) {
+  let message = input.shift().split('');
+
+  let i = 0;
+  let line = input[i];
+
+  while (line !== 'Decode') {
+    let commands = line.split('|');
+    if (commands[0] === 'Move') {
+      move(commands[1])
+    } else if (commands[0] === 'Insert') {
+      insert(commands[1], commands[2])
+    } else if (commands[0] === 'ChangeAll') {
+      changeAll(commands[1], commands[2])
+    }
+
+    i++
+    line = input[i];
+  }
+
+  function move(moves) {
+    moves = Number(moves)
+    for (let i = 0; i < moves; i++) {
+      let tempLetter = message.shift();
+      message.push(tempLetter)
+    }
+  }
+
+  function insert(index, symbol) {
+    index = Number(index)
+    message.splice(index, 0, symbol)
+  }
+
+  function changeAll(toChange, toChangeWith) {
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] === toChange) {
+        message.splice(i, 1, toChangeWith)
+      }
+    }
+  }
+
+  console.log(`The decrypted message is: ${message.join('')}`);
+}
+
+theImitationGame(['zzHe', 'ChangeAll|z|l', 'Insert|2|o', 'Move|3', 'Decode']);
