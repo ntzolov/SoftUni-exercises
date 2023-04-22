e1634193ad33cc780721b87b6b8b281cc7d11ddc
ntzolov <ntzolov@gmail.com>
2023-04-23 00:46:05 +0300

    Update

---
+++ 03.JavaScript advanced/01.JavaScript advanced/04.(S)DOM Introduction/01.(S)DOM Introduction lab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,8 @@
+function calc() {
+  let num1 = document.getElementById('num1').value;
+  let num2 = document.getElementById('num2').value;
+  let sum = document.getElementById('sum');
+
+  sum.value = Number(num1) + Number(num2);
+
+}
