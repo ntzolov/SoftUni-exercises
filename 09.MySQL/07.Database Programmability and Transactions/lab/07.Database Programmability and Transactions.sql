aac48379429a52255e54702e4a57622ffa519e22
ntzolov <ntzolov@gmail.com>
2024-02-01 20:12:21 +0200

    Update 07.Database Programmability and Transactions.sql

--- 09.MySQL/07.Database Programmability and Transactions/lab/07.Database Programmability and Transactions.sql
+++ 09.MySQL/07.Database Programmability and Transactions/lab/07.Database Programmability and Transactions.sql
@@ -22,12 +22,17 @@ DELIMITER ;
 
 DELIMITER $
 
-CREATE PROCEDURE usp_raise_salaries(department_name VARCHAR(50));
-BEGIN 
-	UPDATE employees e
-	JOIN departments d ON e.department_id = d.department_id
-	SET e.salary = e.salary * 1.05
-	WHERE d.name = department_name;
+CREATE PROCEDURE usp_raise_salaries(department_name varchar(50))
+BEGIN
+UPDATE
+  employees AS e
+JOIN departments AS d
+ON
+  e.department_id = d.department_id
+SET
+  salary = salary * 1.05
+WHERE
+  d.name = department_name;
 END $
 
 DELIMITER ;
@@ -36,6 +41,64 @@ SELECT e.employee_id, e.first_name, e.salary FROM employees e
 JOIN departments d ON d.department_id = e.department_id
 WHERE d.name = 'Finance'
 ORDER BY first_name, salary;
+
+/*****3*****/
+
+DELIMITER $$
+
+CREATE PROCEDURE usp_raise_salary_by_id(id INT)
+BEGIN
+START TRANSACTION;
+IF((SELECT count(employee_id) FROM employees WHERE employee_id
+like id)<>1) THEN
+ROLLBACK;
+ELSE
+UPDATE employees AS e SET salary = salary + salary*0.05
+WHERE e.employee_id = id;
+END IF;
+END $$
+
+DELIMITER ;
+
+/*****4*****/
+
+DELIMITER $$
+
+CREATE TABLE deleted_employees(
+employee_id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(20),
+last_name VARCHAR(20),
+middle_name VARCHAR(20),
+job_title VARCHAR(50),
+department_id INT,
+salary DOUBLE
+);
+CREATE TRIGGER tr_deleted_employees
+AFTER DELETE
+ON employees
+FOR EACH ROW
+BEGIN
+INSERT INTO deleted_employees (first_name,last_name,
+middle_name,job_title,department_id,salary)
+VALUES(OLD.first_name,OLD.last_name,OLD.middle_name,
+OLD.job_title,OLD.department_id,OLD.salary);
+END $$
+
+DELIMITER ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
